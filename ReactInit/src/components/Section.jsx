c3949d50f74544c5e181a308af5693967da5cac1
ensacom2019 <asd2590@naver.com>
2026-01-19 19:11:09 +0900

    first commit

---
+++ ReactInit/src/components/Section.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Section = ({ number, style }) => {
+  //console.log(props);
+  return (
+    <>
+      <section className="section" style={style}>
+        Section {number}
+      </section>
+    </>
+  );
+};
+
+export default Section;
---
+++ ReactInit/src/components/subPage.jsx
@@ -0,0 +1,9 @@
+const Sub = () => {
+  return (
+    <>
+      <h2>Sub Page</h2>
+    </>
+  );
+};
+
+export default Sub;
