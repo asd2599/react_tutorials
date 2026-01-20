69f701b5416074fb76014dfe0bd3e9dd28ecbf64
ensacom2019 <asd2590@naver.com>
2026-01-20 19:36:51 +0900

    25_1_20

---
+++ react_hooks/src/components/UseRef.jsx
@@ -0,0 +1,33 @@
+import { useRef, useState } from 'react';
+
+//const ref = useRef(value);
+//current : vlaue
+//state와 마찬가지로 특정 값을 저장하고 유지할 때 사용 : hook은 함수이고 이는 매번 랜더링 될 때마다 재호출하게 된다. 이때 상태 변환 없이 값만 유지하고 싶을 때 사용한다.
+
+const UseRef = () => {
+  const [count, setCount] = useState(0);
+  const countRef = useRef(0);
+  // console.log(countRef.current);
+
+  const increaseState = () => {
+    setCount(count + 1);
+  };
+
+  const increaseRef = () => {
+    countRef.current++;
+  };
+
+  console.log('rendering');
+
+  return (
+    <div>
+      <p>State: {count}</p>
+      <p>Ref: {countRef.current}</p>
+      <button onClick={increaseState}>State Up</button>
+      <br />
+      <button onClick={increaseRef}>Ref Up</button>
+    </div>
+  );
+};
+
+export default UseRef;
---
+++ react_tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <h2 className="text-3xl font-bold underline bg-red-100">
+        Trailwind Test
+      </h2>
+    </>
+  );
+};
+
+export default App;
